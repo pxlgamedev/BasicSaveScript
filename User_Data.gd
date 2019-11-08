95cf8a0f134d1a1d9cc92dd8797bdb1ad73561e1
pxlgamedev <57509900+pxlgamedev@users.noreply.github.com>
2019-11-08 06:52:33 -0500

    Update User_Data.gd

--- User_Data.gd
+++ User_Data.gd
@@ -6,15 +6,13 @@ extends Node
 
 # all variables we want to keep between levels can be stored in a dicitonary
 # access them from anywhere with User_Data.store.[YourVariable]
-# we can also back up the entire dictionary in one go
+# we will back up the entire dictionary in one go, no need to modify the save and load functions
 
 var store = {
 	# you can add as many variables as you want here, they will all be saved
 	level = 1,
 	score = 0,
-	lives = 3,
-	rate = 0.55,
-	ammo1 = 1
+	lives = 3
 	}
 
 	# we want a blank dictionary for use when starting a new game
@@ -23,16 +21,16 @@ var newgame = {
 
 func _ready():
 	# make newgame a copy of our starting variables
-	newgame = store
-	
-	# check at the start if a Saves directory exists.
+	newgame = store	# you could also load a specific json file instead
+	# check if a Saves directory exists.
 	var dir = Directory.new()
 	if !dir.dir_exists("user://Saves"):
 		# if not we'll create it.
 		dir.open("user://")
 		dir.make_dir("user://Saves")
 
-# we'll setup quicksave and quickload buttons here:
+# we'll setup quicksave and quickload buttons here for demonstration
+# they could be called from anywhere with User_Data.save_game_state("filename"):
 func _physics_process(delta):
 	if Input.is_action_just_pressed("qsave"):
 		# Trigger the save function with the filename we want for our save
@@ -61,21 +59,18 @@ func load_game_state(var saveName):
 	var loadGame = File.new()
 	# check that the file exists before trying to open it
 	if !loadGame.file_exists("user://Saves/"+saveName+".sve"):
-		print ("Abort! Abort! No file found...")
+		print ("Abort! Abort! No file to load...")
 		return
+	
 	# time to read the data in our file
 	loadGame.open("user://Saves/"+saveName+".sve", File.READ)
-	# use a new dictionary to parse through the file
-	var loadData = parse_json(loadGame.get_line())
-	# now simply overwrite store with the data from loadData
-	store = loadData
+	# now simply overwrite store by parsing the loaded data
+	store = parse_json(loadGame.get_line())
 	loadGame.close()
+	#Done!
 	
 	# DEBUG stuff: we'll just check that everything worked right
-	print("Level ", store.level)
-	print("Score ", store.score)
-	print("Lives ", store.lives)
-	print("Bounce Ammo ", store.ammo1)
+	print("Loaded data: ", store)
 
 # this function is for resetting your variables for a new game
 func new_game():
